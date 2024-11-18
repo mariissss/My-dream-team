2678f84bfd4892f4c86ba7bb32089d6d7b3f5d78
Afraid-of-Women <bogdan.lukav@gmail.com>
2024-11-18 20:16:26 +0200

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/bohdan.png"} alt="Bohdan Lukavetskyi" style={styles.photo} />
         <div style={styles.textContainer}>
           <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <p style={styles.position}>UI/UX designer</p>
         </div>
       </div>
     </main>
