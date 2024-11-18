84ca4d23ee13984a51a1cad72b8effd6705cce14
Марія Литвин <maria11.ukr@gmail.com>
2024-11-18 17:53:58 +0400

    Added name

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,7 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/avatar-default.png"} alt="Maria Lytvyn" style={styles.photo} />
         <div style={styles.textContainer}>
           <h2 style={styles.name}>John Doe</h2>
           <p style={styles.position}>Software engineer</p>
